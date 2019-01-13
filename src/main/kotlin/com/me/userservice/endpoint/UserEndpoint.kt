ea0d3b685bf3905873dd87eea5ba1c50c9d05c87
J. Eduardo V. Morango <edvmorango@gmail.com>
2019-01-12 23:06:46 -0200

    feature: create user endpoint

--- src/main/kotlin/com/me/userservice/endpoint/UserEndpoint.kt
+++ src/main/kotlin/com/me/userservice/endpoint/UserEndpoint.kt
@@ -1,17 +1,29 @@
 package com.me.userservice.endpoint
 
+import com.me.userservice.endpoint.request.UserRequest
+import com.me.userservice.endpoint.response.UserResponse
+import com.me.userservice.endpoint.response.asResponse
 import com.me.userservice.service.UserService
+import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
-import org.springframework.web.reactive.function.server.body
+import org.springframework.web.reactive.function.server.bodyToFlux
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
-import reactor.core.publisher.toMono
 
 class UserEndpoint(private val userService: UserService) {
 
     fun create(req : ServerRequest): Mono<ServerResponse> {
-        return ServerResponse.ok().build()
+
+        val stream = req.bodyToFlux<UserRequest>()
+                .map{ it.asDomain() }
+                .flatMap(userService::create)
+                .map { it.asResponse() }
+
+        return ServerResponse
+                .ok()
+                .contentType(MediaType.APPLICATION_STREAM_JSON)
+                .body(stream, UserResponse::class.java)
     }
 
     fun findByUuid(req: ServerRequest): Mono<ServerResponse> {

c947a52ad5de79d04e6518da6f3ae2d3a08870a1
J. Eduardo V. Morango <edvmorango@gmail.com>
2019-01-13 16:10:48 -0200

    feature:  of dynamodb

--- src/main/kotlin/com/me/userservice/UserServiceApplication.kt
+++ src/main/kotlin/com/me/userservice/UserServiceApplication.kt
@@ -1,13 +1,22 @@
 package com.me.userservice
 
+import com.me.userservice.config.DynamoDBConfig
 import com.me.userservice.config.context
+import org.springframework.beans.factory.getBean
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.ApplicationContextInitializer
 import org.springframework.context.support.GenericApplicationContext
 
 @SpringBootApplication
-class UserServiceApplication
+class UserServiceApplication(val dynamoDBConfig: DynamoDBConfig) : ApplicationRunner {
+
+	override fun run(args: ApplicationArguments?) {
+		dynamoDBConfig.createTable()
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<UserServiceApplication>(*args)
--- src/main/kotlin/com/me/userservice/config/DynamoDBConfig.kt
+++ src/main/kotlin/com/me/userservice/config/DynamoDBConfig.kt
@@ -7,6 +7,8 @@ import software.amazon.awssdk.auth.credentials.AwsCredentials
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient
+import software.amazon.awssdk.services.dynamodb.model.*
 import java.net.URI
 
 
@@ -36,5 +38,49 @@ class DynamoDBConfig {
                 .build()
 
     }
+
+    fun createTable() {
+
+        val credentials: AwsCredentials = AwsBasicCredentials.create(accessKey, secretKey)
+
+        val client = DynamoDbClient.builder()
+                .endpointOverride(URI.create(host!!))
+                .region(Region.of(region!!))
+                .credentialsProvider { credentials }
+                .build()
+
+        if(!client.listTables().tableNames().contains("user")) {
+
+            val key = KeySchemaElement
+                    .builder()
+                    .keyType(KeyType.HASH)
+                    .attributeName("uuid")
+                    .build()
+
+            val att = AttributeDefinition
+                    .builder()
+                    .attributeName("uuid")
+                    .attributeType(ScalarAttributeType.S)
+                    .build()
+
+            val pt = ProvisionedThroughput
+                    .builder()
+                    .readCapacityUnits(10)
+                    .writeCapacityUnits(10)
+                    .build()
+
+            val req = CreateTableRequest
+                    .builder()
+                    .tableName("user")
+                    .keySchema(key)
+                    .attributeDefinitions(att)
+                    .provisionedThroughput(pt)
+                    .build()
+
+
+            client.createTable(req)
+        }
+    }
+
 
 }

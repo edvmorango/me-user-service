1e95f6c17eb65880b2869a3932c539a362a1ff3e
J. Eduardo V. Morango <edvmorango@gmail.com>
2019-01-17 14:34:35 -0200

    feature: delete at health endpoint spec

--- src/test/kotlin/com/me/userservice/integration/UserEndpointSpec.kt
+++ src/test/kotlin/com/me/userservice/integration/UserEndpointSpec.kt
@@ -4,6 +4,7 @@ import com.me.userservice.endpoint.request.AddressRequest
 import com.me.userservice.endpoint.request.UserRequest
 import com.me.userservice.endpoint.response.UserResponse
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.http.MediaType
 import org.springframework.test.web.reactive.server.expectBodyList
@@ -25,191 +26,233 @@ class UserEndpointSpec: IntegrationBaseSpec() {
             listOf("92991179136"))
 
 
-    @Test
-    @DisplayName("Should create and find user")
-    fun test(){
+    @Nested
+    @DisplayName("SuccessfulCalls")
+    inner class SuccessUserSpec {
+        @Test
+        @DisplayName("Should create and find user")
+        fun test() {
 
-        val created = client
-                .post()
-                .uri("$contextPath/user")
-                .contentType(MediaType.APPLICATION_JSON)
-                .syncBody(validRequestUser)
-                .exchange()
-                .expectStatus()
-                .isCreated
+            val created = client
+                    .post()
+                    .uri("$contextPath/user")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .syncBody(validRequestUser)
+                    .exchange()
+                    .expectStatus()
+                    .isCreated
 
-        val returnResult = created.returnResult<UserResponse>().responseBody.blockFirst()!!
+            val returnResult = created.returnResult<UserResponse>().responseBody.blockFirst()!!
 
-        client
-                .get()
-                .uri("$contextPath/user/${returnResult.uuid}")
-                .exchange()
-                .expectStatus()
-                .isOk
+            client
+                    .get()
+                    .uri("$contextPath/user/${returnResult.uuid}")
+                    .exchange()
+                    .expectStatus()
+                    .isOk
 
-    }
+        }
 
-    @Test
-    @DisplayName("Should create many users at same time and list")
-    fun test2(){
+        @Test
+        @DisplayName("Should create many users at same time and list")
+        fun test2() {
 
-        val validRequestUser1 =  validRequestUser.copy(cpf = "87179318949", emails = listOf("somemail1@mail.com"))
+            val validRequestUser1 = validRequestUser.copy(cpf = "87179318949", emails = listOf("somemail1@mail.com"))
 
-        val validRequestUser2 = validRequestUser.copy(cpf = "53131541253", emails = listOf("somemail2@mail.com"))
-        val body = listOf(validRequestUser1, validRequestUser2)
+            val validRequestUser2 = validRequestUser.copy(cpf = "53131541253", emails = listOf("somemail2@mail.com"))
+            val body = listOf(validRequestUser1, validRequestUser2)
 
-        client
-                .post()
-                .uri("$contextPath/user")
-                .contentType(MediaType.APPLICATION_JSON)
-                .syncBody(body)
-                .exchange()
-                .expectStatus()
-                .isCreated
+            client
+                    .post()
+                    .uri("$contextPath/user")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .syncBody(body)
+                    .exchange()
+                    .expectStatus()
+                    .isCreated
 
-        client
-                .get()
-                .uri("$contextPath/user/")
-                .exchange()
-                .expectStatus()
-                .isOk
-                .expectBodyList<UserResponse>().hasSize(3)
-
-
-    }
-
-    @Test
-    @DisplayName("Should create, update and find a updated user ")
-    fun test3(){
-
-        val validUserNew = validRequestUser.copy(cpf = "42260415342", emails = listOf("mmail@mail.com"))
-
-        val created = client
-                .post()
-                .uri("$contextPath/user")
-                .contentType(MediaType.APPLICATION_JSON)
-                .syncBody(validUserNew)
-                .exchange()
-                .expectStatus()
-                .isCreated
-
-        val returnResult = created.returnResult<UserResponse>().responseBody.blockFirst()!!
-
-        val updated =  client
-                .put()
-                .uri("$contextPath/user/${returnResult.uuid}")
-                .contentType(MediaType.APPLICATION_JSON)
-                .syncBody(validUserNew.copy(firstName = "new firstname"))
-                .exchange()
-                .expectStatus()
-                .isOk
-
-        val returnResultUpdated = updated.returnResult<UserResponse>().responseBody.blockFirst()!!
+            client
+                    .get()
+                    .uri("$contextPath/user/")
+                    .exchange()
+                    .expectStatus()
+                    .isOk
+                    .expectBodyList<UserResponse>().hasSize(3)
 
 
-        val expectBody = client
-                .get()
-                .uri("$contextPath/user/${returnResult.uuid}")
-                .exchange()
-                .expectStatus()
-                .isOk
-                .expectBody(UserResponse::class.java).returnResult()
+        }
 
-        assert(expectBody.responseBody!! == returnResultUpdated)
+        @Test
+        @DisplayName("Should create, update and find a updated user ")
+        fun test3() {
 
-    }
+            val validUserNew = validRequestUser.copy(cpf = "42260415342", emails = listOf("mmail@mail.com"))
 
-    @Test
-    @DisplayName("Should create many users at same time and list for firstName")
-    fun test4(){
+            val created = client
+                    .post()
+                    .uri("$contextPath/user")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .syncBody(validUserNew)
+                    .exchange()
+                    .expectStatus()
+                    .isCreated
 
-        val validRequestUser1 =  validRequestUser.copy(firstName = "John" ,cpf = "64284761188", emails = listOf("somemail1@4mail.com"))
+            val returnResult = created.returnResult<UserResponse>().responseBody.blockFirst()!!
 
-        val validRequestUser2 = validRequestUser.copy(firstName = "John", cpf = "61183327897", emails = listOf("somemail2@4mail.com"))
-        val body = listOf(validRequestUser1, validRequestUser2)
+            val updated = client
+                    .put()
+                    .uri("$contextPath/user/${returnResult.uuid}")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .syncBody(validUserNew.copy(firstName = "new firstname"))
+                    .exchange()
+                    .expectStatus()
+                    .isOk
 
-        client
-                .post()
-                .uri("$contextPath/user")
-                .contentType(MediaType.APPLICATION_JSON)
-                .syncBody(body)
-                .exchange()
-                .expectStatus()
-                .isCreated
-
-        client
-                .get()
-                .uri("$contextPath/user?firstName=${validRequestUser1.firstName}")
-                .exchange()
-                .expectStatus()
-                .isOk
-                .expectBodyList<UserResponse>().hasSize(2)
+            val returnResultUpdated = updated.returnResult<UserResponse>().responseBody.blockFirst()!!
 
 
-    }
+            val expectBody = client
+                    .get()
+                    .uri("$contextPath/user/${returnResult.uuid}")
+                    .exchange()
+                    .expectStatus()
+                    .isOk
+                    .expectBody(UserResponse::class.java).returnResult()
+
+            assert(expectBody.responseBody!! == returnResultUpdated)
+
+        }
+
+        @Test
+        @DisplayName("Should create many users at same time and list for firstName")
+        fun test4() {
+
+            val validRequestUser1 = validRequestUser.copy(firstName = "John", cpf = "64284761188", emails = listOf("somemail1@4mail.com"))
+
+            val validRequestUser2 = validRequestUser.copy(firstName = "John", cpf = "61183327897", emails = listOf("somemail2@4mail.com"))
+            val body = listOf(validRequestUser1, validRequestUser2)
+
+            client
+                    .post()
+                    .uri("$contextPath/user")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .syncBody(body)
+                    .exchange()
+                    .expectStatus()
+                    .isCreated
+
+            client
+                    .get()
+                    .uri("$contextPath/user?firstName=${validRequestUser1.firstName}")
+                    .exchange()
+                    .expectStatus()
+                    .isOk
+                    .expectBodyList<UserResponse>().hasSize(2)
 
 
-    @Test
-    @DisplayName("Should create many users at same time and list for lastName")
-    fun test5(){
-
-        val validRequestUser1 =  validRequestUser.copy(lastName = "Doe" ,cpf = "16498430460", emails = listOf("doe@5mail.com"))
-
-        val validRequestUser2 = validRequestUser.copy(lastName = "Doe", cpf = "31765057795", emails = listOf("doe2@5mail.com"))
-
-        val validRequestUser3 = validRequestUser.copy(cpf = "85737421279", emails = listOf("somemail@5mail.com"))
-
-        val body = listOf(validRequestUser1, validRequestUser2, validRequestUser3)
-
-        client
-                .post()
-                .uri("$contextPath/user")
-                .contentType(MediaType.APPLICATION_JSON)
-                .syncBody(body)
-                .exchange()
-                .expectStatus()
-                .isCreated
-
-        client
-                .get()
-                .uri("$contextPath/user?lastName=${validRequestUser1.lastName}")
-                .exchange()
-                .expectStatus()
-                .isOk
-                .expectBodyList<UserResponse>().hasSize(2)
+        }
 
 
-    }
+        @Test
+        @DisplayName("Should create many users at same time and list for lastName")
+        fun test5() {
+
+            val validRequestUser1 = validRequestUser.copy(lastName = "Doe", cpf = "16498430460", emails = listOf("doe@5mail.com"))
+
+            val validRequestUser2 = validRequestUser.copy(lastName = "Doe", cpf = "31765057795", emails = listOf("doe2@5mail.com"))
+
+            val validRequestUser3 = validRequestUser.copy(cpf = "85737421279", emails = listOf("somemail@5mail.com"))
+
+            val body = listOf(validRequestUser1, validRequestUser2, validRequestUser3)
+
+            client
+                    .post()
+                    .uri("$contextPath/user")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .syncBody(body)
+                    .exchange()
+                    .expectStatus()
+                    .isCreated
+
+            client
+                    .get()
+                    .uri("$contextPath/user?lastName=${validRequestUser1.lastName}")
+                    .exchange()
+                    .expectStatus()
+                    .isOk
+                    .expectBodyList<UserResponse>().hasSize(2)
 
 
-    @Test
-    @DisplayName("Should create many users at same time and list for cpf")
-    fun test6(){
-
-        val validRequestUser1 =  validRequestUser.copy(cpf = "47536562179", emails = listOf("doe@6mail.com"))
-
-        val validRequestUser2 = validRequestUser.copy(cpf = "67166992360", emails = listOf("doe2@6mail.com"))
-
-        val body = listOf(validRequestUser1, validRequestUser2)
-
-        client
-                .post()
-                .uri("$contextPath/user")
-                .contentType(MediaType.APPLICATION_JSON)
-                .syncBody(body)
-                .exchange()
-                .expectStatus()
-                .isCreated
-
-        client
-                .get()
-                .uri("$contextPath/user?cpf=${validRequestUser1.cpf}")
-                .exchange()
-                .expectStatus()
-                .isOk
-                .expectBodyList<UserResponse>().hasSize(1)
+        }
 
 
+        @Test
+        @DisplayName("Should create many users at same time and list for cpf")
+        fun test6() {
+
+            val validRequestUser1 = validRequestUser.copy(cpf = "47536562179", emails = listOf("doe@6mail.com"))
+
+            val validRequestUser2 = validRequestUser.copy(cpf = "67166992360", emails = listOf("doe2@6mail.com"))
+
+            val body = listOf(validRequestUser1, validRequestUser2)
+
+            client
+                    .post()
+                    .uri("$contextPath/user")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .syncBody(body)
+                    .exchange()
+                    .expectStatus()
+                    .isCreated
+
+            client
+                    .get()
+                    .uri("$contextPath/user?cpf=${validRequestUser1.cpf}")
+                    .exchange()
+                    .expectStatus()
+                    .isOk
+                    .expectBodyList<UserResponse>().hasSize(1)
+
+
+        }
+
+
+        @Test
+        @DisplayName("Should create, find, delete and list deleted user")
+        fun test7() {
+
+            val validUserNew = validRequestUser.copy(cpf = "42554626669", emails = listOf("mmail@7mail.com"))
+
+            val created = client
+                    .post()
+                    .uri("$contextPath/user")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .syncBody(validUserNew)
+                    .exchange()
+                    .expectStatus()
+                    .isCreated
+
+            val returnResult = created.returnResult<UserResponse>().responseBody.blockFirst()!!
+
+
+            client
+                    .delete()
+                    .uri(  "$contextPath/user/${returnResult.uuid}")
+                    .exchange().expectStatus()
+                    .isNoContent
+
+            client
+                    .get()
+                    .uri("$contextPath/user?cpf=${returnResult.cpf}")
+                    .exchange()
+                    .expectStatus()
+                    .isOk
+                    .expectBodyList<UserResponse>().hasSize(0)
+
+
+
+
+        }
     }
 
 }
